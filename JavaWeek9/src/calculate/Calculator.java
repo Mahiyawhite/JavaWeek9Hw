cda27af01adc4baec9a1c9f0d2ce220459057217
Mahiwhite <vdprajapti23@gmail.com>
2024-05-01 21:44:57 +0100

    my first push to repo

---
+++ JavaWeek9/src/calculate/Calculator.java
@@ -0,0 +1,42 @@
+package calculate;
+
+public class Calculator {
+    public void addition(int a, int b) {
+        int result = (a + b);
+        System.out.println("addition " + result);
+    }
+
+    public void subtraction(int a, int b) {
+        int result = (a - b);
+        System.out.println("subtraction " + result);
+    }
+
+    public void multiplication(int a, int b) {
+        int result = (a * b);
+        System.out.println("multiplication " + result);
+    }
+
+    public void division(int a, int b) {
+        int result = (a / b);
+        System.out.println("division " + result);
+    }
+
+    public void calculateResult(int a, int b, char symbol) {
+        switch (symbol) {
+            case '+':
+                addition(a, b);
+                break;
+            case '-':
+                subtraction(a, b);
+                break;
+            case '*':
+                multiplication(a, b);
+                break;
+            case '/':
+                division(a, b);
+                break;
+            default:
+                System.out.println("Invalid symbol.");
+        }
+    }
+}
---
+++ JavaWeek9/src/calculate/Main.java
@@ -0,0 +1,22 @@
+package calculate;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Please enter one of symbol +,-,*,/: ");
+        char symbol = scan.next().charAt(0);
+
+        System.out.print("Enter second Number: ");
+        int num2 = scan.nextInt();
+
+        calculator.calculateResult(num1, num2, symbol);
+
+    }
+}
