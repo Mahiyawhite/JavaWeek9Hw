bacf3280dc61234e6e5104b2768a7bfa98fc2829
Mahiwhite <vdprajapti23@gmail.com>
2024-05-09 23:28:09 +0100

    my first push to repo

--- JavaWeek9/src/week8javaprogramclass4/que23methodoverriding23/Bike2.java
+++ JavaWeek9/src/week8javaprogramclass4/que23methodoverriding23/Bike2.java
@@ -5,7 +5,6 @@ public class Bike2 extends Vehicle {
     public void run() {
         System.out.println("Bike is running safely");
     }
-
     public static void main(String[] args) {
         Bike2 b = new Bike2();
         b.run();
